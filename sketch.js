3e67ccaae426369fdecfc3f1fb72539f32ae9ed6
NicMotta <34142261+NicMotta@users.noreply.github.com>
2021-05-18 21:51:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,8 +9,6 @@ function touchStarted() {
   } else {
     value = 0;
   }
+  
+  window.navigator.vibrate(200);
 }
-
-
-window.navigator.vibrate(200); // vibrate for 200ms
-window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
